3bf7e0a998d0cb9875449f765691e9a32258748b
JungInBaek <bji1527@naver.com>
2023-09-15 08:46:27 +0900

    test : String 학습테스트
    - split(), substring(), charAt() 학습테스트
    - .gitmessage.txt 파일 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,42 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String[] actual1 = "1,2".split(",");
+        assertThat(actual1).contains("1");
+        assertThat(actual1).contains("2");
+
+        String[] actual2 = "1".split(",");
+        assertThat(actual2).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        String abc = "abc";
+
+        char actual1 = abc.charAt(0);
+        assertThat(actual1).isEqualTo('a');
+
+        assertThatThrownBy(
+                () -> abc.charAt(4)
+        ).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
 }
